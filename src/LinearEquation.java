6bdeef4af935e5b40dcdfe8c5d63b1f17a2bca41
ChrisChris501 <Christanahagbekeye@gmail.com>
2025-09-30 16:53:57 +0100

    Comparison Operators

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -574,7 +574,11 @@ public class LinearEquation {
 
         */
 
-        Math.pow(2, 2);
-        Math.exp(1);
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        double x = input.nextDouble();
+        double y = input.nextDouble();
+        double z = input.nextDouble();
+        System.out.println((x < y && y < z) ? "sorted" : "not sorted");
+
     }
     }
