be3097876ab72c1f9442ea527902b706cd8322ac
ChrisChris501 <Christanahagbekeye@gmail.com>
2025-10-30 15:02:43 +0000

    Loops

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -921,6 +921,8 @@ public class LinearEquation {
         System.out.printf("%-10d %-10f %-10f %-10f %-10f\n", 30, 0.5236, 0.5000, 0.8660, 0.5773 );
         System.out.printf("%-10d %-10f %-10f %-10f %-10f\n", 60, 1.0472, 0.8660, 0.5000, 1.7320);
 
+        //Loop
+        
     }
     }
 
