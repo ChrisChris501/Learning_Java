b6aaf31fd704ba4e81b4301836d68fe5ba4460e5
ChrisChris501 <Christanahagbekeye@gmail.com>
2025-12-08 12:54:27 +0000

    Converting miles to kilometers

--- src/Conversions_MilesToKilometers.java
+++ src/Conversions_MilesToKilometers.java
@@ -1,5 +1,10 @@
 public class Conversions_MilesToKilometers {
     public static void main(String[] args) {
+        double miles = 100;
+        final double KILOMETERS_PER_MILE = 1.609;
+        double kilometers = miles * KILOMETERS_PER_MILE;
+        System.out.println(kilometers);
+        System.out.println (Math.pow(2, 3.5));
 
     }
 }
--- src/IntroToJava.java
+++ src/IntroToJava.java
@@ -14,11 +14,6 @@ public class IntroToJava {
         y = (a * f - e * c) / (a * d - b * c);
         System.out.println("x is equals " + x + " and y is equals " + y);
 
-        double miles = 100;
-        final double KILOMETERS_PER_MILE = 1.609;
-        double kilometers = miles * KILOMETERS_PER_MILE;
-        System.out.println(kilometers);
-        System.out.println (Math.pow(2, 3.5));
 
         int x = 2;
         int y = 1;
