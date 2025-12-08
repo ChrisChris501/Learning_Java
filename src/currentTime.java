c4e0629f7d912ae26cadebce07d0ef0aadf977b8
ChrisChris501 <Christanahagbekeye@gmail.com>
2025-12-08 12:29:53 +0000

    Calculating current time in Java

--- src/currentTime.java
+++ src/currentTime.java
@@ -1,24 +1,26 @@
 public class currentTime {
-    //Current time display
-    long totalMilliseconds = System.currentTimeMillis();
+    public static void main(String[] args) {
+        //Current time display
+        long totalMilliseconds = System.currentTimeMillis();
         System.out.println("totalMilli is " + totalMilliseconds);
 
-    long totalSeconds = totalMilliseconds / 1000;
+        long totalSeconds = totalMilliseconds / 1000;
         System.out.println("totalSec is " + totalSeconds);
 
-    long currentSeconds = totalSeconds % 60 ;
+        long currentSeconds = totalSeconds % 60;
         System.out.println("currentSecs is " + currentSeconds);
 
-    long totalMinutes = totalSeconds / 60;
-         System.out.println("totalMin is " + totalMinutes);
+        long totalMinutes = totalSeconds / 60;
+        System.out.println("totalMin is " + totalMinutes);
 
-    long currentMinutes = totalMinutes % 60;
+        long currentMinutes = totalMinutes % 60;
         System.out.println("currentMin " + currentMinutes);
 
-    long totalHours = totalMinutes / 60;
+        long totalHours = totalMinutes / 60;
         System.out.println("totalMins is " + totalMinutes);
 
-    long currentHour = totalHours % 24 ;
+        long currentHour = totalHours % 24;
         System.out.println("Current time is " + currentHour + "hour " + currentMinutes + "mins " + currentSeconds + "secs " + "GMT");
 
+    }
 }
