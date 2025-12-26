1913f4635303e07eae0db20706d28e775f724621
ChrisChris501 <Christanahagbekeye@gmail.com>
2025-12-26 13:22:08 +0000

    If/Else Statement

---
+++ src/If_Else_Statements.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class If_Else_Statements {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // IF statement
+        System.out.print("Enter a score: ");
+        double score1 = input.nextDouble();
+        double percentageIncrement = 0.03 * score1;
+
+        if (score1 >= 90) {
+            System.out.println(score1 + percentageIncrement);
+        }
+
+        // IF / ELSE statement
+        System.out.print("Enter a score: ");
+        double score2 = input.nextDouble();
+
+        double threePercent = 0.03 * score2;
+        double onePercent = 0.01 * score2;
+
+        if (score2 >= 90) {
+            System.out.println(score2 + threePercent);
+        } else {
+            System.out.println(score2 + onePercent);
+        }
+
+        // Check if number is divisible by 10
+        System.out.print("Enter any number: ");
+        int number1 = input.nextInt();
+        boolean divisibleByTen = number1 % 10 == 0;
+        System.out.println(divisibleByTen);
+
+        // Even or multiple of 5
+        System.out.print("Enter any number: ");
+        int number2 = input.nextInt();
+
+        if (number2 % 2 == 0) {
+            System.out.println(number2 + " is even");
+        } else if (number2 % 5 == 0) {
+            System.out.println(number2 + " is multiple of 5");
+        }
+
+        // BMI Calculator
+        final double UNDER_WEIGHT = 18.5;
+        final double NORMAL_WEIGHT = 25.0;
+        final double OVER_WEIGHT = 30.0;
+
+        final double POUND_TO_KG = 0.45359237;
+        final double INCH_TO_METER = 0.0254;
+
+        System.out.print("Enter your weight in pounds: ");
+        double weightPounds = input.nextDouble();
+
+        System.out.print("Enter your height in inches: ");
+        double heightInches = input.nextDouble();
+
+        double weightKg = weightPounds * POUND_TO_KG;
+        double heightMeters = heightInches * INCH_TO_METER;
+
+        double bmi = weightKg / (heightMeters * heightMeters);
+        System.out.println("BMI is " + bmi);
+
+        if (bmi < UNDER_WEIGHT) {
+            System.out.println("You are underweight, work on it");
+        } else if (bmi < NORMAL_WEIGHT) {
+            System.out.println("Your weight is normal, great!");
+        } else if (bmi < OVER_WEIGHT) {
+            System.out.println("You are overweight, work on it");
+        } else {
+            System.out.println("You are obese!");
+        }
+
+        // Age check
+        System.out.print("What is your age? ");
+        double age = input.nextDouble();
+
+        if (age > 13 && age < 18) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        input.close();
+    }
+}
